9feca0ba523cd8a6053af657adec2b8925c662a8
Paul Doran <paul.doran@autotrader.co.uk>
2020-07-27 21:57:23 +0100

    adds basic DocumentHighlighter

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,12 @@
 import * as vscode from 'vscode';
 import { DbtDefinitionProvider, DBT_MODE } from './dbtDefinitionProvider';
+import { DbtDocumentHighlightProvider } from './dbtDocumentHighlightProvider';
 
 
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
-		vscode.languages.registerDefinitionProvider(DBT_MODE, new DbtDefinitionProvider())
+		vscode.languages.registerDefinitionProvider(DBT_MODE, new DbtDefinitionProvider()),
+		vscode.languages.registerDocumentHighlightProvider(DBT_MODE, new DbtDocumentHighlightProvider())
 	);
 }
 
